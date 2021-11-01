4ce9d376f35c2c8933bebc1f0beaaa24f2875082
azzzy93 <boss-312@mail.ru>
2021-11-01 15:43:27 +0600

    Initial commit

---
+++ app/src/main/java/kg/geektech/home_work_s4_e1/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package kg.geektech.home_work_s4_e1.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/kg/geektech/home_work_s4_e1/ui/profile/ProfileFragment.java
@@ -0,0 +1,59 @@
+package kg.geektech.home_work_s4_e1.ui.profile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kg.geektech.home_work_s4_e1.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initListeners();
+    }
+
+    private void initListeners() {
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Uri uri = result.getData().getData();
+                        binding.imageView.setImageURI(uri);
+                    }
+                });
+
+        binding.imageView.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            activityResultLauncher.launch(intent);
+        });
+
+    }
+}
